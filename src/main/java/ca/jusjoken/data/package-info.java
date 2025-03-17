9b20089637126e82a1809d3b3b723570892e506c
start.vaadin.com <no-reply@vaadin.com>
2025-03-17 13:13:46 +0000

    Generated project

---
+++ src/main/java/ca/jusjoken/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package ca.jusjoken.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/ca/jusjoken/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package ca.jusjoken.services;
+
+import org.springframework.lang.NonNullApi;
